3bb6446b2540f1efea327db59f4c0c41a06d3c33
Pedro Fortes <pedro.daniel.g@gmail.com>
2020-01-26 15:51:13 +0100

    Bech32 addresses implementation. PEP8 corrections.

--- README.md
+++ README.md
@@ -2,23 +2,23 @@
 Bitcoin Wallet Address Generator
 
 This is a simple Bitcon wallet address generator coded in Python 3.
-It generates a Private Key in different formats (hex, wif and compressed wif) and corresponding Public Addresses, raw, P2WPKH addresses starting with prefix 1 and P2SH addresses starting with prefix 3 as part of Segwit soft fork.
+It generates a Private Key in different formats (hex, wif and compressed wif) and corresponding Public Addresses, raw, P2WPKH addresses starting with prefix 1, P2SH addresses starting with prefix 3 as part of Segwit soft fork and Bech32 addresses with prefix bc1 P2WPKH and P2WSH.
 
-* soon Bench32 addresses starting with prefix bc1
 
 # Usage:
 python wallet.py
 
 # Output:
 ```
-Private Key HEX: 37450b5785141a486208d6b58774d4e9f1c7915257c0d578e795c587bb6ac4f7
-Private Key WIF: 5JEdNXQuhB4uqLMARdEhX5TJGtFKXjszjknqVeTX3EdZMFVRHU2
-Private Key WIF compressed: Ky59Y1EXaKSGCLPzbgwTqkf5F7CCngG5qiHK3SL3asAMnGEAeFG2
+Private Key HEX: 562997b9379008a97d7e4cbf6f19c41d4c348de96199aba44c79e3b32f1cc174
+Private Key WIF: 5JUEV9pjH4o16NRTBsaN18ehDNaqSsD66EySbT1BZsSvYQttmga
+Private Key WIF compressed: Kz7CXFwfbuk3QhYvazEFhamNVcGLqAcMU8g7y3moe5fRKZXZaj7k
 
-Public Key: 0408834b6c3744bc8f286ede05166f3ef75810642cdf65fd0cbc5cf6916a5da605c67f1e641c4b07c4dd79a6a52509aa5264c3596930b3baa42355ca83be39ce24
-Public Key compressed: 0208834b6c3744bc8f286ede05166f3ef75810642cdf65fd0cbc5cf6916a5da605
+Public Key: 04e7ba7ecb0fccaae6bbe1d1493a1dc2e6d266006374411af3ff82546490ffef67cd2e06d0e385b695fe2eaf436d61559837571d989b413e75d763127f628a05f3
+Public Key compressed: 03e7ba7ecb0fccaae6bbe1d1493a1dc2e6d266006374411af3ff82546490ffef67
 
-Public Address 1: 19iPwjvPUZfHfTSGnVmcEhjiAvKiDYNxfp
-Public Address 3: 331qUgRgz1AyzzRqo3ZLGkeJcGZQbUhtDp
-Public Address bc1: 
+Public Address 1: 1oKgBdYNyNiW5ntjEpJwsJwRZ1Qr9cPce
+Public Address 3: 3K55C3EdCJYZWoc95Tt7CbXnUkDSaVeZrC
+Public Address bc1 (P2WPKH): bc1q363sj8rhw0uqmxurraa0rtydhp32cyuke4lfwt 
+Public Address bc1 (P2WSH): bc1qsy38c4na9fm0y5pvyhja0hscpx5my290pgwgeq5eduvdqmejkwyq0whxpw
 ```
--- wallet.py
+++ wallet.py
@@ -1,151 +1,155 @@
-import time
+import binascii
 import hashlib
-import string
+import random
+import time
 import base58
 import ecdsa
-import random
-import math
-import binascii
 
-""" Bitcoin Wallet Generator v0.2
+import segwit_addr
+
+""" Bitcoin Wallet Generator v0.3
 
          Pedro Fortes - 05/2018
+         Pedro Fortes - 01/2020 - Bech32 implementation
 
          https://bitcoincore.org/en/segwit_wallet_dev/
-         https://pypi.python.org/pypi/ecdsa - ECDSA manual"""
+         https://pypi.python.org/pypi/ecdsa - ECDSA manual
+         https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#Reference_implementations - BIP 173
+         """
+
 
 class Keys:
 
-        def __init__(self):
-            self.privkeyhex  = ''
-            self.privkeywif = ''
-            self.privkeywifcompressed = ''
-            
-            self.pubkey   = ''
-            self.pubkeycompressed   = ''
-            self.pubaddr1 = ''
-            self.pubaddr3 = ''
-            self.pubaddrbc1 = ''
- 
- 
-        def seed(self):                                         
-            t = int(time.time())
-            return str(random.getrandbits(3000) - t)
-                        
+    def __init__(self):
+        self.privkeyhex = ''
+        self.privkeywif = ''
+        self.privkeywifcompressed = ''
 
-        def privatekey(self, seed=None):
-                
-            if(seed==None): seed = self.seed() 
-            self.privkeyhex = hashlib.sha256(seed.encode())
-                
-         
-        def privatekeywif(self):
- 
-            prefix = b'\x80'
-            suffix = b'\x01'
-            
-            d = prefix + self.privkeyhex.digest()     
-     
-            checksum   = self.doublehash256(d).digest()[:4]
-            checksum_c = self.doublehash256(d + suffix).digest()[:4]
+        self.pubkey = ''
+        self.pubkeycompressed = ''
+        self.pubaddr1 = ''
+        self.pubaddr3 = ''
+        self.pubaddrbc1_P2WPKH = ''
+        self.pubaddrbc1_P2WSH = ''
 
-            self.privkeywif = base58.b58encode(d + checksum) 
-            self.privkeywifcompressed = base58.b58encode(d + suffix + checksum_c) 
-         
+    def seed(self):
+        t = int(time.time())
+        return str(random.getrandbits(3000) - t)
 
-        def hash160(self, v):
-            r = hashlib.new('ripemd160')
-            r.update(hashlib.sha256(v).digest())
-            return r
+    def privatekey(self, seed=None):
+        if (seed == None): seed = self.seed()
+        self.privkeyhex = hashlib.sha256(seed.encode())
 
+    def privatekeywif(self):
+        prefix = b'\x80'
+        suffix = b'\x01'
 
-        def doublehash256(self, v):
-            return hashlib.sha256(hashlib.sha256(v).digest())
+        d = prefix + self.privkeyhex.digest()
 
+        checksum = self.doublehash256(d).digest()[:4]
+        checksum_c = self.doublehash256(d + suffix).digest()[:4]
 
-        def ecdsaSECP256k1(self, digest):
-            # SECP256k1 - Bitcoin elliptic curve 
-            sk = ecdsa.SigningKey.from_string(digest, curve=ecdsa.SECP256k1)                    
-            return sk.get_verifying_key()
+        self.privkeywif = base58.b58encode(d + checksum).decode('utf-8')
+        self.privkeywifcompressed = base58.b58encode(d + suffix + checksum_c).decode('utf-8')
 
-                                
-        def publicaddress1(self):
+    def hash160(self, v):
+        r = hashlib.new('ripemd160')
+        r.update(hashlib.sha256(v).digest())
+        return r
 
-            prefix_a = b'\x04'
-            prefix_b = b'\x00'
+    def doublehash256(self, v):
+        return hashlib.sha256(hashlib.sha256(v).digest())
 
-            digest = self.privkeyhex.digest()
-                                  
-            p = prefix_a + self.ecdsaSECP256k1(digest).to_string() # 1 + 32 bytes + 32 bytes
-            self.pubkey = str(binascii.hexlify(p).decode('utf-8'))
-                            
-            hash160 = self.hash160(p)
+    def ecdsaSECP256k1(self, digest):
+        # SECP256k1 - Bitcoin elliptic curve
+        sk = ecdsa.SigningKey.from_string(digest, curve=ecdsa.SECP256k1)
+        return sk.get_verifying_key()
 
-            m = prefix_b + hash160.digest()
-            checksum = self.doublehash256(m).digest()[:4]        
-        
-            self.pubaddr1 = base58.b58encode(m + checksum)                  
-             
+    def publicaddress1(self):
+        prefix_a = b'\x04'
+        prefix_b = b'\x00'
 
-        def publicaddress3(self):
- 
-            prefix_even = b'\x02'
-            prefix_odd = b'\x03'
-            prefix_a = prefix_odd
-            prefix_b = b'\x05'
-            prefix_redeem = b'\x00\x14'
+        digest = self.privkeyhex.digest()
 
+        p = prefix_a + self.ecdsaSECP256k1(digest).to_string()  # 1 + 32 bytes + 32 bytes
+        self.pubkey = str(binascii.hexlify(p).decode('utf-8'))
 
-            digest = self.privkeyhex.digest()
-            
-            ecdsa_digest = self.ecdsaSECP256k1(digest).to_string()
- 
-            x_coord = ecdsa_digest[:int(len(ecdsa_digest)/2)]
-            y_coord = ecdsa_digest[int(len(ecdsa_digest)/2):]            
+        hash160 = self.hash160(p)
 
-            if (int(binascii.hexlify(y_coord),16) % 2 == 0): prefix_a = prefix_even
-            
-            p = prefix_a + x_coord
-            
-            self.pubkeycompressed = str(binascii.hexlify(p).decode('utf-8'))
+        m = prefix_b + hash160.digest()
+        checksum = self.doublehash256(m).digest()[:4]
 
- 
-            redeem_script = self.hash160(prefix_redeem + self.hash160(p).digest()).digest() # 20 bytes
- 
-            m = prefix_b + redeem_script
-            checksum = self.doublehash256(m).digest()[:4]        
-        
-            self.pubaddr3 = base58.b58encode(m + checksum)
+        self.pubaddr1 = base58.b58encode(m + checksum).decode('utf-8')
 
+    def _generate_publickey(self):
+        prefix_even = b'\x02'
+        prefix_odd = b'\x03'
+        prefix_a = prefix_odd
 
+        digest = self.privkeyhex.digest()
 
-        def publicaddressbc1(self):
+        ecdsa_digest = self.ecdsaSECP256k1(digest).to_string()
 
-            # soon     
-        
-            self.pubaddrbc1 = ''          
-            
-                                        
-        def generate(self, seed=None):
+        x_coord = ecdsa_digest[:int(len(ecdsa_digest) / 2)]
+        y_coord = ecdsa_digest[int(len(ecdsa_digest) / 2):]
 
-            self.privatekey(seed)
-            self.privatekeywif()
-            
-            self.publicaddress1()
-            self.publicaddress3()
-            self.publicaddressbc1()
+        if (int(binascii.hexlify(y_coord), 16) % 2 == 0): prefix_a = prefix_even
 
-        def __str__(self):
-                
-            return '\nPrivate Key HEX: {0}\nPrivate Key WIF: {1}\nPrivate Key WIF compressed: {2}\n\nPublic Key: {3}\nPublic Key compressed: {4}\n\nPublic Address 1: {5}\nPublic Address 3: {6}\nPublic Address bc1: {7}' \
-             .format(self.privkeyhex.hexdigest(), self.privkeywif, self.privkeywifcompressed, self.pubkey, self.pubkeycompressed, self.pubaddr1, self.pubaddr3, self.pubaddrbc1)
+        p = prefix_a + x_coord
+
+        self.pubkeycompressed = str(binascii.hexlify(p).decode('utf-8'))
+
+        return p
+
+    def publicaddress3(self):
+        p = self._generate_publickey()
+
+        prefix_redeem = b'\x00\x14'
+        prefix_b = b'\x05'
+
+        redeem_script = self.hash160(prefix_redeem + self.hash160(p).digest()).digest()  # 20 bytes
+
+        m = prefix_b + redeem_script
+        checksum = self.doublehash256(m).digest()[:4]
+
+        self.pubaddr3 = base58.b58encode(m + checksum).decode('utf-8')
+
+    def publicaddressbc1(self):
+        p = self._generate_publickey()
+
+        redeem_script_P2WPKH = self.hash160(p).digest()  # 20 bytes
+        redeem_script_P2WSH = hashlib.sha256(p).digest()
+
+        self.pubaddrbc1_P2WPKH = str(segwit_addr.encode('bc', 0x00, redeem_script_P2WPKH))
+        self.pubaddrbc1_P2WSH = str(segwit_addr.encode('bc', 0x00, redeem_script_P2WSH))
+
+    def generate(self, seed=None):
+        self.privatekey(seed)
+        self.privatekeywif()
+
+        self.publicaddress1()
+        self.publicaddress3()
+        self.publicaddressbc1()
+
+    def __str__(self):
+        return """
+Private Key HEX: %s
+Private Key WIF: %s
+Private Key WIF compressed: %s
+
+Public Key: %s
+Public Key compressed: %s
+
+Public Address 1: %s
+Public Address 3: %s
+Public Address bc1 (P2WPKH): %s 
+Public Address bc1 (P2WSH): %s
+""" % (self.privkeyhex.hexdigest(), self.privkeywif, self.privkeywifcompressed, self.pubkey,
+       self.pubkeycompressed, self.pubaddr1, self.pubaddr3, self.pubaddrbc1_P2WPKH, self.pubaddrbc1_P2WSH)
 
 
 if __name__ == "__main__":
-        
-        wallet = Keys()
-        
-        wallet.generate()
-           
-        print(wallet)
-  
+    wallet = Keys()
+    wallet.generate()
+
+    print(wallet)
